4c16b7bb9abc11aa94d8650257b269e17df0e697
jclin@articulate.com <jclin@articulate.com>
2015-10-14 19:02:03 -0700

    Implement insertion and removal into FavoriteMovies table

--- app/src/main/java/com/jclin/popularmovies/fragments/MovieDetailsActivityFragment.java
+++ app/src/main/java/com/jclin/popularmovies/fragments/MovieDetailsActivityFragment.java
@@ -2,15 +2,20 @@ package com.jclin.popularmovies.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import com.jclin.popularmovies.R;
+import com.jclin.popularmovies.activities.MovieDetailsActivity;
+import com.jclin.popularmovies.data.FavoriteMovieQuery;
 import com.jclin.popularmovies.data.ImageProvider;
 import com.jclin.popularmovies.data.ImageSize;
 import com.jclin.popularmovies.data.Movie;
@@ -18,41 +23,74 @@ import com.jclin.popularmovies.data.TheMovieDBUri;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnCheckedChanged;
 
 public class MovieDetailsActivityFragment extends Fragment
 {
+    private static final String LOG_TAG     = MovieDetailsActivity.class.getName();
+    private static final String MOVIE_KEY   = "MOVIE_BUNDLE_KEY";
+
     protected @Bind(R.id.textView_movie_title)   TextView _movieTitleTextView;
     protected @Bind(R.id.textView_release_date)  TextView _releaseDateTextView;
     protected @Bind(R.id.textView_user_rating)   TextView _userRatingTextView;
     protected @Bind(R.id.textView_plot_synopsis) TextView _plotSynopsisTextView;
+    protected @Bind(R.id.switch_favorite)        Switch   _switchFavorite;
 
     protected @Bind(R.id.root_linear_layout) LinearLayout _rootLinearLayout;
+
+    private Movie _movie;
 
     public MovieDetailsActivityFragment()
     {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState)
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         View view = inflater.inflate(R.layout.fragment_movie_details, container, false);
 
         ButterKnife.bind(this, view);
 
-        Movie movie = getMovieFromIntent();
+        _movie = getMovieFromIntent(savedInstanceState);
 
-        populateControls(movie);
+        populateControls(_movie);
 
         _rootLinearLayout
             .getViewTreeObserver()
-            .addOnGlobalLayoutListener(new ImageViewHeightAdjuster(_rootLinearLayout, movie));
+            .addOnGlobalLayoutListener(new ImageViewHeightAdjuster(_rootLinearLayout, _movie));
 
         return view;
     }
 
-    private Movie getMovieFromIntent()
+    @Override
+    public void onSaveInstanceState(Bundle outState)
     {
+        super.onSaveInstanceState(outState);
+
+        outState.putParcelable(MOVIE_KEY, _movie);
+    }
+
+    @OnCheckedChanged(R.id.switch_favorite)
+    protected void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+    {
+        if (isChecked)
+        {
+            FavoriteMovieQuery.insert(_movie, getActivity().getContentResolver());
+        }
+        else
+        {
+            FavoriteMovieQuery.remove(_movie, getActivity().getContentResolver());
+        }
+    }
+
+    private Movie getMovieFromIntent(Bundle savedInstanceState)
+    {
+        if (savedInstanceState != null)
+        {
+            Log.i(LOG_TAG, "Restoring Movie instance...");
+            _movie = savedInstanceState.getParcelable(MOVIE_KEY);
+        }
+
         return getActivity()
             .getIntent()
             .getParcelableExtra(getString(R.string.INTENT_DATA_MOVIE));
@@ -64,6 +102,7 @@ public class MovieDetailsActivityFragment extends Fragment
         _releaseDateTextView.setText(movie.getReleaseDateString());
         _userRatingTextView.setText(String.format("%.1f", movie.getVoteAverage()));
         _plotSynopsisTextView.setText(movie.getOverview());
+        _switchFavorite.setChecked(FavoriteMovieQuery.alreadyExists(movie, getActivity().getContentResolver()));
     }
 
     private final class ImageViewHeightAdjuster implements ViewTreeObserver.OnGlobalLayoutListener
